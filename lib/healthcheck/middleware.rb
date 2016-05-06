6f5d1eb271782e4d5a17303f12637dee49d6a1f9
Asfand Qazi <ayqazi@gmail.com>
2016-05-06 11:59:58 +0100

    V1 of healthcheck gem

---
+++ lib/healthcheck/middleware.rb
@@ -0,0 +1,19 @@
+class Healthcheck::Middleware
+
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    if env['PATH_INFO'] == '/healthcheck'
+      if Healthcheck.passed?
+        [200, {}, ['OK']]
+      else
+        [500, {}, ['Healthcheck Failed']]
+      end
+    else
+      @app.call(env)
+    end
+  end
+
+end
